14f2cfec6d03f41325a01db9d5a8a226ec17d80c
enmeneo <allan.castillocruz09@gmail.com>
2024-04-05 18:13:33 -0400

    a ver si ahora si

--- NominaCRUD/Controllers/PagoesController.cs
+++ NominaCRUD/Controllers/PagoesController.cs
@@ -68,6 +68,7 @@ namespace NominaCRUD.Controllers
             //return View(await recorverNominaContext.ToListAsync());
         }
 
+
         // GET: Pagoes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
